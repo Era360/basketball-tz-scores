ebf27d095a3ef3841869185600b83e944fa3a45d
Elia <mkumboelia@gmail.com>
2023-02-13 22:44:44 +0300

    Added some team logos for visualizing
    - ABC
    - JKT
    and Pazi

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,5 +1,9 @@
 import back_basket from "./basketball-banner.jpg"
+import jkt_logo from "./JKT.png"
+import pazi_logo from "./pazi.png"
 
 export {
-    back_basket
+    back_basket,
+    jkt_logo,
+    pazi_logo
 }
